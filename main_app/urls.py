5835ba233a635d6f56c0352e24bd20f8bfa93c3c
mbanov <m.banov7@gmail.com>
2020-05-01 18:02:45 +0200

    registration

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.Register.as_view(), name='register'),
     path('<int:question_id>/', views.question, name='question')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from main_app.forms import CustomUserCreationForm
 
 
 def index(request):
@@ -8,3 +12,16 @@ def index(request):
 
 def question(request, question_id):
     return HttpResponse("Question %s" % question_id)
+
+
+class Register(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/register.html'
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect(reverse('index'))
