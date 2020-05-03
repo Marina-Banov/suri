c4313ece05be893fdad8d76a4266c728f8abc39d
mbanov <m.banov7@gmail.com>
2020-05-03 13:12:13 +0200

    remove bootstrap js

--- suri/settings.py
+++ suri/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/bootstrap-4.3.1-dist')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/bootstrap-4.3.1-css')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
