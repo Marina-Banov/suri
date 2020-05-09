6afd95f78fc56c6ca1e8601b9f9d578aaed2d38d
mbanov <m.banov7@gmail.com>
2020-05-09 14:36:11 +0200

    added fontawesome icons

--- suri/settings.py
+++ suri/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/bootstrap-4.3.1-css')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/bootstrap-4.3.1-css'),
+    os.path.join(BASE_DIR, 'static/fontawesome-free-5.13.0-web')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
